146fc1fdfcf29b728e35832460304306d1f2409d
mmiah <mmiah00@stuy.edu>
2019-03-12 13:47:37 -0400

    testing

--- Quick.java
+++ Quick.java
@@ -26,8 +26,43 @@ public class Quick {
      }
      return 0;
   }
+
+  public static void deBugPartition (int [] data, int start, int end) {
+     Random rng = new Random ();
+     int r = Math.abs (rng.nextInt () % data.length);
+     int pivot = data [r];
+     System.out.println("Pivot Value: " + pivot);
+     while (start != end) {
+       int now = data [start];
+       if (now < pivot) {
+         start ++;
+       }
+       if (now > pivot) {
+         int e = data [end];
+         data [end] = now;
+         data [start] = e;
+         end --;
+       }
+       if (start == end) {
+         data [start] = pivot;
+         data [r] = now;
+         System.out.println (start);
+         //return start;
+       }
+     }
+     System.out.println (toString (data));
+  }
+
+  public static String toString (int[] data) {
+    String ans = "[ ";
+    for (int i =0 ; i < data.length; i ++) {
+      ans += data[i] + " ";
+    }
+    return ans + " ]";
+  }
+
   public static void main (String [] args) {
     int[] test = {8, 6, 7, 5, 3, 0, 9};
-    System.out.println (partition (test, 0, 6));
+    deBugPartition (test, 0, 6);
   }
 }
