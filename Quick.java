4f8c32f9f10b78551fee5f0a86287a750da6bf50
mmiah00 <mmiah00@stuty.edu>
2019-03-12 14:50:37 -0400

    writing partition

--- Quick.java
+++ Quick.java
@@ -29,33 +29,35 @@ public class Quick {
 
   public static void deBugPartition (int [] data, int start, int end) {
     System.out.println (toString (data));
-     Random rng = new Random ();
-     int r = Math.abs (rng.nextInt () % data.length);
-     int pivot = data [r];
-     System.out.println("Pivot Value: " + pivot);
-     int temp = data[start];
-     data[r] = temp;
-     data[0] = pivot;
-     start ++; 
-     while (start < data.length && start != end ) {
-       if (start == end) {
-         int s = data [start];
-         data [start] = pivot;
-         data [r] = s;
-         System.out.println ("Answer: " + start);
-         //return r;
-       }
-       if (data[start] < pivot) {
-         start ++;
-       }
-       else {
-         int e = data [end];
-         int now = data[start];
-         data[end] = now;
-         data [start] = e;
-         end --;
-       }
-     }
+    Random rng = new Random ();
+    int pivIndex = Math.abs (rng.nextInt () % data.length);
+    int pivot = data [pivIndex];
+    System.out.println ("Pivot: " + pivot);
+    int temp = data[start];
+    data [pivIndex] = temp;
+    data [start] = pivot;
+    start ++;
+
+    while (start < data.length && end >= 0) {
+      if (start == end) {
+        int now = data[start];
+        data [0] = now;
+        data[start] = pivot;
+        System.out.println ("Ans: " + start);
+        break;
+      }
+      if (data[start] > pivot) {
+        int now = data[start];
+        int endVal = data[end];
+        data [start] = endVal; //swapping end and beginning
+        data [end] = now;
+        end --;
+      }
+      if (data[start] < pivot) {
+        start ++;
+      }
+    }
+
      /*
      while (start != end) {
        int now = data [start];
@@ -92,3 +94,33 @@ public class Quick {
     deBugPartition (test, 0, 6);
   }
 }
+
+/*
+Random rng = new Random ();
+int r = Math.abs (rng.nextInt () % data.length);
+int pivot = data [r];
+System.out.println("Pivot Value: " + pivot);
+int temp = data[start];
+data[r] = temp;
+data[0] = pivot;
+start ++;
+while (start < data.length && start != end ) {
+  if (start == end) {
+    int s = data [start];
+    data [start] = pivot;
+    data [r] = s;
+    System.out.println ("Answer: " + start);
+    //return r;
+  }
+  if (data[start] < pivot) {
+    start ++;
+  }
+  else {
+    int e = data [end];
+    int now = data[start];
+    data[end] = now;
+    data [start] = e;
+    end --;
+  }
+}
+*/
