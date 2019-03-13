c0e1a800d3c4f512e0b71c68174034d88de1a06f
mmiah <mmiah00@stuy.edu>
2019-03-13 08:28:30 -0400

    C:/Program Files/Git/

--- Quick.java
+++ Quick.java
@@ -28,7 +28,7 @@ public class Quick {
   }
 
   public static void deBugPartition (int [] data, int start, int end) {
-    System.out.println (toString (data));
+    System.out.println ("Initial: " + toString (data));
     Random rng = new Random ();
     int pivIndex = Math.abs (rng.nextInt () % data.length);
     int pivot = data [pivIndex];
@@ -58,36 +58,76 @@ public class Quick {
         end --;
       }
     }
+     System.out.println ("Final: " + toString (data));
+  }
 
-     /*
-     while (start != end) {
-       int now = data [start];
-       if (now < pivot) {
-         start ++;
-       }
-       if (now > pivot) {
-         int e = data [end];
-         data [end] = now;
-         data [start] = e;
-         end --;
-       }
-       if (start == end) {
-         data [start] = pivot;
-         data [r] = now;
-         System.out.println (start);
-         //return start;
-       }
-     }
-     */
-     System.out.println (toString (data));
+  public static int[][] viv (int [] data, int start, int end) {
+    Random rng = new Random();
+    int pivot = rng.nextInt(end-start+1) + start;
+    int index = start;
+
+    int num = data[pivot];
+    data[pivot] = data[start];
+    data[start] = num;
+    start++;
+    boolean moveLeft = true;
+
+    while (start < end) {
+      int temp = data[start];
+      if (data[start] == num) {
+        if (moveLeft) {
+          start++;
+          moveLeft = false;
+        }
+        else {
+          data[start] = data[end];
+          data[end] = temp;
+          end--;
+          moveLeft = true;
+        }
+      }
+      else if (data[start] > num) {
+        data[start] = data[end];
+        data[end] = temp;
+        end--;
+      }
+      else {
+        start++;
+      }
+    }
+
+    while (index < data.length-1 && data[index+1] <= num) {
+      index++;
+    }
+
+    data[0] = data[index];
+    data[index] = num;
+    int[][] newData = new int[2][];
+    newData[0] = data;
+    newData[1] = new int[] {index};
+
+    return newData;
   }
 
   public static String toString (int[] data) {
-    String ans = "[ ";
+    String ans = "[";
     for (int i =0 ; i < data.length; i ++) {
-      ans += data[i] + " ";
+      ans += data[i];
+      if (i != data.length - 1) {
+        ans += " ";
+      }
     }
     return ans + "]";
+  }
+
+  public static String toString (int[][] data) {
+    String ans = "[ ";
+    for (int i =0 ; i < data.length; i ++) {
+      ans += toString (data[i]);
+      ans += "\n";
+    }
+    ans += "]";
+    return ans;
   }
 
   public static void main (String [] args) {
@@ -100,7 +140,7 @@ public class Quick {
     deBugPartition (a, 0, a.length - 1);
     System.out.println ();
     deBugPartition (b, 0, b.length - 1);
-    System.out.println (); 
+    System.out.println ();
     deBugPartition (c, 0, c.length - 1);
   }
 }
