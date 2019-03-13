4f928fbb1521e9752186236a683a2178a9d0769b
mmiah <mmiah00@stuy.edu>
2019-03-13 08:32:42 -0400

    added text from debug to partition

--- Quick.java
+++ Quick.java
@@ -3,28 +3,34 @@ import java.util.*;
 public class Quick {
 
   public static int partition (int [] data, int start, int end) {
-     Random rng = new Random ();
-     int r = Math.abs (rng.nextInt () % data.length);
-     int pivot = data [r];
-     while (start < data.length && end >= 0) {
-       if (start == end) {
-         int s = data [start];
-         data [start] = pivot;
-         data [r] = s;
-         return r;
-       }
-       if (data[start] < pivot) {
-         start ++;
-       }
-       else {
-         int e = data [end];
-         int now = data[start];
-         data[end] = now ;
-         data [start] = e;
-         end --;
-       }
-     }
-     return 0;
+    Random rng = new Random ();
+    int pivIndex = Math.abs (rng.nextInt () % data.length);
+    int pivot = data [pivIndex];
+    int temp = data[start];
+    data [pivIndex] = temp;
+    data [start] = pivot;
+    start ++;
+
+    while (start < data.length && end >= 0) {
+      if (start == end) {
+        int now = data[start];
+        data[0] = now;
+        data[start] = pivot;
+        return start;
+      }
+      if (data[start] < pivot) {
+        start ++;
+      }
+      else {
+        //if (data[start] > pivot) {
+        int e = data[end];
+        int s = data[start];
+        data [end] = s;
+        data[start] = e;
+        end --;
+      }
+    }
+    return 0;
   }
 
   public static void deBugPartition (int [] data, int start, int end) {
@@ -60,8 +66,8 @@ public class Quick {
     }
      System.out.println ("Final: " + toString (data));
   }
-  
-  public static String toString (int[] data) {
+
+  private static String toString (int[] data) {
     String ans = "[";
     for (int i =0 ; i < data.length; i ++) {
       ans += data[i];
@@ -72,7 +78,7 @@ public class Quick {
     return ans + "]";
   }
 
-  public static String toString (int[][] data) {
+  private static String toString (int[][] data) {
     String ans = "[ ";
     for (int i =0 ; i < data.length; i ++) {
       ans += toString (data[i]);
@@ -87,12 +93,13 @@ public class Quick {
     int [] a = {2,4,7,1,3,6,12};
     int [] b = {40,3,25,24,30};
     int [] c = {1,3,8,5,7,2};
-    deBugPartition (test, 0, 6);
+    //deBugPartition (test, 0, 6);
+    System.out.println (partition (test, 0, 6));
     System.out.println ();
-    deBugPartition (a, 0, a.length - 1);
+    System.out.println (partition (a, 0, a.length - 1));
     System.out.println ();
-    deBugPartition (b, 0, b.length - 1);
+    System.out.println (partition (b, 0, b.length - 1));
     System.out.println ();
-    deBugPartition (c, 0, c.length - 1);
+    System.out.println (partition (c, 0, c.length - 1));
   }
 }
