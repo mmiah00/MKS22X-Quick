487c0fa6a2610c7074fd20c1071a3e7b73494560
mmiah <mmiah00@stuy.edu>
2019-03-12 23:11:48 -0400

    kinda working

--- Quick.java
+++ Quick.java
@@ -49,7 +49,8 @@ public class Quick {
       if (data[start] < pivot) {
         start ++;
       }
-      if (data[start] > pivot) {
+      else {
+        //if (data[start] > pivot) {
         int e = data[end];
         int s = data[start];
         data [end] = s;
@@ -91,36 +92,15 @@ public class Quick {
 
   public static void main (String [] args) {
     int[] test = {8, 6, 7, 5, 3, 0, 9};
+    int [] a = {2,4,7,1,3,6,12};
+    int [] b = {40,3,25,24,30};
+    int [] c = {1,3,8,5,7,2};
     deBugPartition (test, 0, 6);
+    System.out.println ();
+    deBugPartition (a, 0, a.length - 1);
+    System.out.println ();
+    deBugPartition (b, 0, b.length - 1);
+    System.out.println (); 
+    deBugPartition (c, 0, c.length - 1);
   }
 }
-
-/*
-Random rng = new Random ();
-int r = Math.abs (rng.nextInt () % data.length);
-int pivot = data [r];
-System.out.println("Pivot Value: " + pivot);
-int temp = data[start];
-data[r] = temp;
-data[0] = pivot;
-start ++;
-while (start < data.length && start != end ) {
-  if (start == end) {
-    int s = data [start];
-    data [start] = pivot;
-    data [r] = s;
-    System.out.println ("Answer: " + start);
-    //return r;
-  }
-  if (data[start] < pivot) {
-    start ++;
-  }
-  else {
-    int e = data [end];
-    int now = data[start];
-    data[end] = now;
-    data [start] = e;
-    end --;
-  }
-}
-*/
