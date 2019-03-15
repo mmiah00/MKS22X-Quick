9d647ad02fad03dbd5d89691bb974787409101f7
mmiah <mmiah00@stuy.edu>
2019-03-15 01:10:57 -0400

    fixed all bugs in quicksort

--- Quick.java
+++ Quick.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Quick {
 
   public static void quicksort(int[] data) {
-    quicksort (data, 0, data.length - 1);
+    quicksort (data, 0, data.length  - 1);
   }
 
   private static void quicksort (int[] data, int lo, int hi) {
@@ -30,44 +30,44 @@ public class Quick {
     return data[div]; //once div == k, that kth value is in the right spot
   }
 
-  private static int partition (int [] data, int start, int end) {
-    //System.out.println ("Initial: " + toString (data));
-    Random rng = new Random ();
-    int pivIndex = Math.abs (rng.nextInt () % data.length);
-    int pivot = data [pivIndex];
-    //System.out.println ("Pivot: " + pivot);
-    int temp = data[start];
-    data [pivIndex] = temp; //switching beginning and pivot
-    data [start] = pivot;
+  private static int partition(int[] data, int start, int end) {
+    Random rng = new Random();
+    int pivot = Math.abs (rng.nextInt(end-start+1)) + start;
+    int og = start; //keeping track of the start
+    int piv = data[pivot]; //pivot value
+    int s = data[og];
+    data[og] = piv; //swapping first value and pivot value
+    data[pivot] = s;
 
-    int i = start + 1; //new variable to keep track of lo end
+    start++;
 
-    while (i != end) {
-      int s = data[i]; //start
-      int e = data[end]; //end
-      if (s > pivot) {
-        data[i] = e; // if bigger than pivot bring it to the end
-        data[end] = s;
-        end --;
+    while(start < end){
+      if(data[start] == piv ) { //if equal swap start and end values and move end up
+        int e = data[end];
+        data[end] = data[start];
+        data[start] = e;
+        end--;
       }
-      else {
-        i ++;
+      if(data[start] > piv ) { //if start is bigger than pivot, swap start and end values and move end up
+        int e = data[end];
+        data[end] = data[start];
+        data[start] = e;
+        end--;
       }
+      else start++; //if smaller move start down
     }
 
-    if (data [i] > pivot) {
-      int x = data[i - 1];
-      data[i - 1] = pivot;
-      data[start] = x;
-      return i - 1;
+    //once start reaches end check if the value there is greater or less than pivot
+    if (piv < data[end]){ //if greater, put it in front
+      data[og] = data[end - 1];
+      data[end -1] = piv;
+      return end-1;
     }
-    else {
-      int x = data[i];
-      data [i] = pivot;
-      data[start] = x;
-      return i;
+    else { //if less than pivot, swap places
+      data[og] = data[end];
+      data[end] = piv;
+      return end;
     }
-    //System.out.println ("Final: " + toString (data));
   }
 
   private static void deBugPartition (int [] data, int start, int end) {
@@ -77,16 +77,23 @@ public class Quick {
     int pivot = data [pivIndex];
     System.out.println ("Pivot: " + pivot);
     int temp = data[start];
-    data [pivIndex] = temp;
+    data [pivIndex] = temp; //switching beginning and pivot
     data [start] = pivot;
 
-    int i = start + 1;
+    int i = start + 1; //new variable to keep track of lo end
 
     while (i != end) {
-      int s = data[i];
-      int e = data[end];
-      if (s > pivot) {
+      int s = data[i]; //start
+      int e = data[end]; //end
+      /*
+      if(s == pivot && Math.random() > 0.5) {
         data[i] = e;
+        data[end] = s;
+        end--;
+      }
+      */
+      if (s > pivot) {
+        data[i] = e; // if bigger than pivot bring it to the end
         data[end] = s;
         end --;
       }
@@ -164,14 +171,25 @@ public class Quick {
     int [] c = {1,3,8,5,7,2};
 
 
+
     /*
     for (int i = 0; i < 10; i ++) {
-      deBugPartition(a, 0, 6);
+      deBugPartition(c, 0, c.length -1 );
       System.out.println ("--------------------------------------------------------");
     }
     */
     quicksort (test);
     System.out.println (toString (test));
+    System.out.println ("--------------------------------------------------------");
+    quicksort (a);
+    System.out.println (toString  (a));
+    System.out.println ("--------------------------------------------------------");
+    quicksort (b);
+    System.out.println (toString  (b));
+    System.out.println ("--------------------------------------------------------");
+    quicksort (c);
+    System.out.println (toString  (c));
+    System.out.println ("--------------------------------------------------------");
 
     /*
     System.out.println (toString (a));
@@ -182,3 +200,52 @@ public class Quick {
 
   }
 }
+
+/*
+private static int partition (int [] data, int start, int end) {
+  //System.out.println ("Initial: " + toString (data));
+  Random rng = new Random ();
+  int pivIndex = (int)(Math.random() * ((end - start) + 1)) + start;
+  int pivot = data [pivIndex];
+  //System.out.println ("Pivot: " + pivot);
+  int temp = data[start];
+  data [pivIndex] = temp; //switching beginning and pivot
+  data [start] = pivot;
+
+  int i = start + 1; //new variable to keep track of lo end
+
+  while (i < end) {
+    int s = data[i]; //start
+    int e = data[end]; //end
+
+    if(s == pivot && Math.random() > 0.5) {
+      data[i] = e;
+      data[end] = s;
+      end--;
+    }
+
+    if (s > pivot) {
+      data[i] = e; // if bigger than pivot bring it to the end
+      data[end] = s;
+      end --;
+    }
+    else {
+      i ++;
+    }
+  }
+
+  if (pivot >= data[end]) {
+    int x = data[end - 1];
+    data[i - 1] = pivot;
+    data[start] = x;
+    return i - 1;
+  }
+  else {
+    int x = data[i];
+    data [i] = pivot;
+    data[start] = x;
+    return i;
+  }
+  //System.out.println ("Final: " + toString (data));
+}
+*/
